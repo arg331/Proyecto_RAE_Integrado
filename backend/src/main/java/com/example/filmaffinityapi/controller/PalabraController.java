ec221006e459221748a61eed09d57dabab9887a7
ABEL RODRIGUEZ GUTIERREZ <arg331@inlumine.ual.es>
2026-04-05 23:49:36 +0200

    feat: Proyecto completo

--- backend/src/main/java/com/example/filmaffinityapi/controller/PalabraController.java
+++ backend/src/main/java/com/example/filmaffinityapi/controller/PalabraController.java
@@ -1,5 +1,4 @@
 package com.example.filmaffinityapi.controller;
-
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,27 +8,22 @@ import com.example.filmaffinityapi.entity.Palabra;
 import com.example.filmaffinityapi.repository.PalabraRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/palabras")
 public class PalabraController {
-
     private final PalabraRepository palabraRepository;
-
     public PalabraController(PalabraRepository palabraRepository) {
         this.palabraRepository = palabraRepository;
     }
-
     @GetMapping
     public Iterable<Palabra> getAllPalabras() {
         return palabraRepository.findAll();
     }
-
     @GetMapping("/search")
     public List<Palabra> searchByPalabra(@RequestParam String palabra) {
         return palabraRepository.findByPalabraContainingIgnoreCase(palabra);
     }
-
     @GetMapping("/fecha")
     public List<Palabra> searchByFecha(@RequestParam String fecha) {
         return palabraRepository.findByFecha(fecha);
